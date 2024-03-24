337f2cc58d0572f6b814da5e7ab9a14e9f0fc637
GimOne <gksk731@naver.com>
2024-03-24 23:47:39 +0900

    [Bronze V] Title: A+B - 7, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    int Count;
+    cin >> Count;
+
+    for (size_t i = 1; i < Count + 1; i++)
+    {
+        int A, B;
+        cin >> A >> B;
+        cout << "Case " << "#" <<i << ": " << A + B << endl;
+    }
+
+    return 0;
+}
