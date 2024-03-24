4697e9c6f352c96a3ef5f987d568c0d20609f534
GimOne <gksk731@naver.com>
2024-03-24 21:01:16 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    long long A, B, C;
+    cin >> A >> B >> C;
+
+    long long Result = A + B + C;
+    cout << Result << endl;
+
+    return 0;
+}
