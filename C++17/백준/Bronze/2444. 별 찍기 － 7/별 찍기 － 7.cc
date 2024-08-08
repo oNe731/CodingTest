3ee4b47644fff1505b2c0071855e3c2784ea8be0
GimOne <gksk731@naver.com>
2024-08-08 21:41:49 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
+++ C++17/백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
@@ -7,19 +7,17 @@ int main()
     int Count;
     cin >> Count;
 
-    int Top   = 2 * Count / 2;
-    int Lower = Top - 1;
-    int All   = Top * 2 - 1;
-    for (int i = 0; i < Top; ++i)
+    int All   = 2 * Count - 1;
+    for (int i = 0; i < Count; ++i)
     {
-        for (int j = 0; j < Lower - i; ++j)
+        for (int j = 0; j < (Count - 1) - i; ++j)
             cout << " ";
         for (int k = 0; k <= i * 2; ++k)
             cout << "*";
         cout << endl;
     }
 
-    for (int i = 0; i < Lower; ++i)
+    for (int i = 0; i < (Count - 1); ++i)
     {
         for (int j = 0; j <= i; ++j)
             cout << " ";
