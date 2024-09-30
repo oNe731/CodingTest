f578f7c2e249735e0b2a0c4b816d0cb6b5152ba9
GimOne <gksk731@naver.com>
2024-09-30 17:33:25 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Bronze/2566. 최댓값/최댓값.cc
+++ C++17/백준/Bronze/2566. 최댓값/최댓값.cc
@@ -4,20 +4,13 @@ using namespace std;
 
 int main()
 {
+    int maxNumber = 0, maxI = 0, maxJ = 0;
     int Numbers[9][9];
     for (size_t i = 0; i < 9; i++)
     {
         for (size_t j = 0; j < 9; j++)
-            cin >> Numbers[i][j];
-    }
-
-    int maxNumber = 0;
-    int maxI = 0;
-    int maxJ = 0;
-    for (size_t i = 0; i < 9; i++)
-    {
-        for (size_t j = 0; j < 9; j++)
         {
+            cin >> Numbers[i][j];
             if (Numbers[i][j] > maxNumber)
             {
                 maxNumber = Numbers[i][j];
