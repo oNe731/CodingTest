3e3f7a5f6260dbcbf2adf9bfcb10201349022b17
GimOne <gimone0731@gmail.com>
2024-12-06 23:23:23 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int A, B, C;
+    cin >> A >> B >> C;
+
+    if(A == 60 && B == 60 && C == 60)
+        cout << "Equilateral" << "\n";
+    else if(A + B + C == 180 && A == B || A + B + C == 180 && A == C || A + B + C == 180 && B == C)
+        cout << "Isosceles" << "\n";
+    else if (A + B + C == 180)
+        cout << "Scalene" << "\n";
+    else
+        cout << "Error" << "\n";
+
+    return 0;
+}
