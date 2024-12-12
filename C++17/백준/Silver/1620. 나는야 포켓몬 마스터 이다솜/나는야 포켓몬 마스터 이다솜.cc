494ee1f33967c5470e93fce58aa299875224770e
GimOne <gimone0731@gmail.com>
2024-12-13 05:22:00 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 144 ms, Memory: 26372 KB -BaekjoonHub

--- C++17/백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
+++ C++17/백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -7,8 +7,9 @@ using namespace std;
 
 int main()
 {
-    ios::sync_with_stdio(false);
+    ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+    cout.tie(NULL);
 
     int CountA, CountB;
     cin >> CountA >> CountB;
