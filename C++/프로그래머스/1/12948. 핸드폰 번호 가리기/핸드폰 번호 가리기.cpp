c981b10b9ff11f53f8e998abeb7d7cc19dae49b0
GimOne <gimone0731@gmail.com>
2025-01-22 21:46:11 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ C++/프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string phone_number) {
+    string answer = "";
+    for (size_t i = 0; i < phone_number.size(); i++)
+    {
+        if (i < phone_number.size() - 4)
+            answer += '*';
+        else
+            answer += phone_number[i];
+    }
+    return answer;
+}
