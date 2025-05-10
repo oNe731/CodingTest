aacf7cd3c2622b46f4086efb91a8fddebfeaded6
GimOne <gimone0731@gmail.com>
2025-05-11 05:51:37 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- C++17/백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
+++ C++17/백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -10,10 +10,10 @@ vector<vector<bool>> Visited;
 int Mx[4] = { -1, 1, 0, 0 };
 int My[4] = { 0, 0, -1, 1 };
 
-void DFS(int X, int Y, int Size, int* Houses)
+void DFS(int X, int Y, int Size, int& Count)
 {
     Visited[X][Y] = true;
-    (*Houses)++;
+    Count++;
     for (size_t i = 0; i < 4; i++)
     {
         int Nx = X + Mx[i];
@@ -24,7 +24,7 @@ void DFS(int X, int Y, int Size, int* Houses)
 
         if (Map[Nx][Ny] == 1 && Visited[Nx][Ny] == false)
         {
-            DFS(Nx, Ny, Size, Houses);
+            DFS(Nx, Ny, Size, Count);
         }
     }
 }
@@ -54,7 +54,7 @@ int main() {
                 Count++;
 
                 int Houses = 0;
-                DFS(i, j, N, &Houses);
+                DFS(i, j, N, Houses);
                 House.push_back(Houses);
             }
         }
