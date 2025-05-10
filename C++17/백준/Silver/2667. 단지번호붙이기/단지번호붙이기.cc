b8f1a4ea267e9a3d450923e1c2e0195e114d9f69
GimOne <gimone0731@gmail.com>
2025-05-11 05:49:08 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ C++17/백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+vector<vector<int>> Map;
+vector<vector<bool>> Visited;
+int Mx[4] = { -1, 1, 0, 0 };
+int My[4] = { 0, 0, -1, 1 };
+
+void DFS(int X, int Y, int Size, int* Houses)
+{
+    Visited[X][Y] = true;
+    (*Houses)++;
+    for (size_t i = 0; i < 4; i++)
+    {
+        int Nx = X + Mx[i];
+        int Ny = Y + My[i];
+
+        if (Nx < 0 || Nx >= Size || Ny < 0 || Ny >= Size)
+            continue;
+
+        if (Map[Nx][Ny] == 1 && Visited[Nx][Ny] == false)
+        {
+            DFS(Nx, Ny, Size, Houses);
+        }
+    }
+}
+
+int main() {
+    int N;
+    cin >> N;
+
+    Map.resize(N, vector<int>(N));
+    Visited.resize(N, vector<bool>(N, false));
+    for (size_t i = 0; i < N; i++)
+    {
+        string Input;
+        cin >> Input;
+        for (size_t j = 0; j < Input.length(); j++)
+            Map[i][j] = Input[j] - '0';
+    }
+
+    int Count = 0;
+    vector<int> House;
+    for (size_t i = 0; i < N; i++)
+    {
+        for (size_t j = 0; j < N; j++)
+        {
+            if (Map[i][j] == 1 && Visited[i][j] == false)
+            {
+                Count++;
+
+                int Houses = 0;
+                DFS(i, j, N, &Houses);
+                House.push_back(Houses);
+            }
+        }
+    }
+
+    cout << Count << "\n";
+    sort(House.begin(), House.end());
+    for (auto iter : House)
+        cout << iter << "\n";
+
+    return 0;
+}
